9adc1aa9a0fb8b1aaede7b25a3988505e5fbfa5c
Tiago Henriques <trinosauro@gmail.com>
2017-09-25 23:45:18 +0200

    Add comment showing how to use classic functions instead of fat-arrow functions

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
 const Hello = () => <div>Welcome to next.js!</div>
 
+// This page is implemented using a "fat-arrow" function,
+// a new, more compact kind of function that was introduced in ES6.
+// You could also have implemented it using classic functions, e.g.
+//
+// const Hello = function() {
+//   return <div>Welcome to next.js!</div>
+// }
+//
+// Also, please note that this page uses the special name index.js.
+// This means you can access it either as localhost:3000/index or just as localhost:3000/
+
 export default Hello
