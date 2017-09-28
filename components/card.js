c40017729c247decc458840b8ca18ce0325756ba
Tiago Henriques <trinosauro@gmail.com>
2017-09-28 18:26:04 +0200

    Implement card component

---
+++ components/card.js
@@ -0,0 +1,13 @@
+const Card = () => (
+  <div className="card">
+    <img src="static/good_boy.jpg" height="200" alt="Avatar" />
+    <div className="container">
+      <h4>
+        <b>Good boy</b>
+      </h4>
+      <p>K9 Agent</p>
+    </div>
+  </div>
+)
+
+export default Card
---
+++ pages/cards.js
@@ -0,0 +1,24 @@
+import Card from "../components/card"
+import Head from "next/head"
+
+const Page = () => (
+  <div>
+    <Head>
+      <title>Cards</title>
+      <link rel="stylesheet" href="static/cards.css" />
+      <meta name="description" content="List of cards" />
+      <meta name="keywords" content="cards" />
+    </Head>
+    <h1>Cards</h1>
+    <div className="card-container">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </div>
+)
+
+export default Page
