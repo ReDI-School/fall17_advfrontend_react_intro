8211fef94d974069d9ca6e31b292df2921d3ca28
Tiago Henriques <trinosauro@gmail.com>
2017-10-16 15:01:42 +0200

    Introduce Material-UI

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ const Hello = () => <div>
         <li><a href="page6">Page 6</a></li>
         <li><a href="page7">Page 7</a></li>
         <li><a href="page8">Page 8</a></li>
+        <li><a href="page9">Page 9</a></li>
     </ul>
     <h2>Using map and flexbox</h2>
     <ul>
---
+++ pages/page9.js
@@ -0,0 +1,35 @@
+import Footer from "../components/footer2"
+import Head from "next/head"
+// Material UI theme
+import lightBaseTheme from "material-ui/styles/baseThemes/lightBaseTheme"
+import getMuiTheme from "material-ui/styles/getMuiTheme"
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+// Material UI components
+import AppBar from "material-ui/AppBar"
+import RaisedButton from "material-ui/RaisedButton"
+
+const Page = () => (
+  <div>
+    <Head>
+      <title>My ninth page!</title>
+      <link rel="stylesheet" href="static/main.css" />
+      <meta name="description" content="Let's make the web great again" />
+      <meta name="keywords" content="sample, react, demo, awesome" />
+    </Head>
+    <p>
+      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+        <div>
+          <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+          <h1>My ninth page!</h1>
+          <div>This page is showing how to use React components from the Material-UI library. We're using an AppBar component at the top, and a RaisedButton below. Before you're able to run this locally, you have to run "npm install" to install the new material-ui dependence.</div>
+          <RaisedButton label="Hi Redi School!" />
+        </div>
+      </MuiThemeProvider>
+    </p>
+  </div>
+)
+
+export default Page
