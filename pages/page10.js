0fd212baff7d7809814de51a45d88e83c4278240
Tiago Henriques <trinosauro@gmail.com>
2017-11-15 22:31:18 +0100

    Format code

--- pages/page10.js
+++ pages/page10.js
@@ -1,70 +1,83 @@
-import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Tabs, Tab } from 'material-ui/Tabs';
-import { Card, CardActions } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import TabContents from '../components/tabs/tabContents';
+import React, { Component } from "react"
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import { Tabs, Tab } from "material-ui/Tabs"
+import { Card, CardActions } from "material-ui/Card"
+import FlatButton from "material-ui/FlatButton"
+import darkBaseTheme from "material-ui/styles/baseThemes/darkBaseTheme"
+import lightBaseTheme from "material-ui/styles/baseThemes/lightBaseTheme"
+import getMuiTheme from "material-ui/styles/getMuiTheme"
+import injectTapEventPlugin from "react-tap-event-plugin"
+import TabContents from "../components/tabs/tabContents"
 
-if (typeof window !== 'undefined') injectTapEventPlugin();
+if (typeof window !== "undefined") injectTapEventPlugin()
 
 class Page10 extends Component {
   constructor() {
-    super();
+    super()
 
-    this.setTheme = this.setTheme.bind(this);
+    this.setTheme = this.setTheme.bind(this)
 
     this.state = {
-      theme: 'light'
+      theme: "light"
     }
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     if (nextState.theme === this.state.theme) {
-      return false;
+      return false
     }
 
-    return true;
+    return true
   }
 
   setTheme(theme) {
-    this.setState({ theme });
+    this.setState({ theme })
   }
 
   getTheme(theme) {
-    if (theme === 'light') {
-      return getMuiTheme(lightBaseTheme);
+    if (theme === "light") {
+      return getMuiTheme(lightBaseTheme)
     }
 
-    if (theme === 'dark') {
-      return getMuiTheme(darkBaseTheme);
+    if (theme === "dark") {
+      return getMuiTheme(darkBaseTheme)
     }
   }
 
   render() {
     return (
-      <MuiThemeProvider muiTheme={this.getTheme(this.state.theme)}>
-        <Card>
-          <Tabs>
-            <Tab onActive={this.setTheme('light')} label='Light Theme (Default)'>
-              <TabContents>
-                <FlatButton label='this is inside the tabs' />
-              </TabContents>
-            </Tab>
-            <Tab onActive={this.setTheme('dark')} label='Dark Theme'>
-              <TabContents />
-            </Tab>
-          </Tabs>
-          <CardActions>
-            <FlatButton label='View Dialog' />
-            <FlatButton label='View Drawer' />
-            <FlatButton label='View Snackbar' />
-          </CardActions>
-        </Card>
-      </MuiThemeProvider>
+      <div>
+        <Head>
+          <title>My tenth page!</title>
+          <link rel="stylesheet" href="static/main.css" />
+          <meta name="description" content="Let's make the web great again" />
+          <meta name="keywords" content="sample, react, demo, awesome" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+        </Head>
+        <MuiThemeProvider muiTheme={this.getTheme(this.state.theme)}>
+          <Card>
+            <Tabs>
+              <Tab
+                onActive={this.setTheme("light")}
+                label="Light Theme (Default)"
+              >
+                <TabContents>
+                  <FlatButton label="this is inside the tabs" />
+                </TabContents>
+              </Tab>
+              <Tab onActive={this.setTheme("dark")} label="Dark Theme">
+                <TabContents />
+              </Tab>
+            </Tabs>
+            <CardActions>
+              <FlatButton label="View Dialog" />
+              <FlatButton label="View Drawer" />
+              <FlatButton label="View Snackbar" />
+            </CardActions>
+          </Card>
+        </MuiThemeProvider>
+      </div>
     )
   }
 }
