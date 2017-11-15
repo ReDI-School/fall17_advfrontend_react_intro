ff3cbf0821a340d894bd3efd14a533a47d3fe2e8
Tiago Henriques <trinosauro@gmail.com>
2017-11-15 22:51:16 +0100

    Add dummy data for java course

--- pages/page12.js
+++ pages/page12.js
@@ -20,81 +20,78 @@ import MenuItem from "material-ui/MenuItem"
 if (typeof window !== "undefined") injectTapEventPlugin()
 
 const courses = {
-  "advfrontend": {
+  advfrontend: {
     title: "Advanced Frontend web development (Javascript)",
     description: "Create and deploy a simple web application using Javascript",
     objective: "After this course, you will be able to:",
-    goals: [
-      "abc",
-      "def",
-      "ghi",
-      "jkl"
-    ],
+    goals: ["abc", "def", "ghi", "jkl"],
     address: "Digitales Lernzentrum, Kemperplatz 1a, 7th floor, 10785, Berlin",
     schedule: "Monday and Thursday 19.00-21.00",
     teachTalk: "TeachTalk: Wednesday 19.00-21.00",
     courseImage: "static/01_course_mobile/gmap.png",
     requirements: ""
   },
-  "java": {
+  java: {
     title: "Java",
-    description: "",
-    address: "",
-    schedule: "",
-    teachTalk: "",
+    description: "the quick brown fox jumps over the lazy goat",
+    objective: "bla bla java objectives bla",
+    goals: ["1st goal", "2nd goal"],
+    address: "some where over the rainbow",
+    schedule: "when pigs fly",
+    teachTalk: "no way José!",
     courseImage: "static/01_course_mobile/gmap.png",
-    requirements: ""
+    requirements: "kindergarten diploma"
   }
 }
 
-const Page = (props) => {
+const Page = props => {
   const courseId = props.url.query.course || "advfrontend"
   const course = courses[courseId]
   return (
-  <div>
-    <Head>
-      <title>My twelfth page!</title>
-      <meta name="description" content="Let's make the web great again" />
-      <meta name="keywords" content="sample, react, demo, awesome" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="stylesheet" href="static/main.css" />
-      <link
-        rel="stylesheet"
-        href="//fonts.googleapis.com/css?family=Roboto:300,400,500"
-      />
-      <meta charSet="utf-8" />
-    </Head>
     <div>
-      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-        <div>
+      <Head>
+        <title>My twelfth page!</title>
+        <meta name="description" content="Let's make the web great again" />
+        <meta name="keywords" content="sample, react, demo, awesome" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="static/main.css" />
+        <link
+          rel="stylesheet"
+          href="//fonts.googleapis.com/css?family=Roboto:300,400,500"
+        />
+        <meta charSet="utf-8" />
+      </Head>
+      <div>
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
           <div>
-            <AppBar
-              title={course.title || "Default Course"}
-              iconClassNameRight="muidocs-icon-navigation-expand-more"
-            >
-              <Drawer docked={false}>
-                <MenuItem value={"/"} primaryText="Home" />
-              </Drawer>
-            </AppBar>
+            <div>
+              <AppBar
+                title={course.title || "Default Course"}
+                iconClassNameRight="muidocs-icon-navigation-expand-more"
+              >
+                <Drawer docked={false}>
+                  <MenuItem value={"/"} primaryText="Home" />
+                </Drawer>
+              </AppBar>
+            </div>
+            <div>
+              <Tabs>
+                <Tab label="COURSES">
+                  <TabCourses course={course} />
+                </Tab>
+                <Tab label="HOMEWORK">
+                  <TabHomework course={course} />
+                </Tab>
+                <Tab label="CALENDAR">
+                  <TabCalendar course={course} />
+                </Tab>
+              </Tabs>
+            </div>
           </div>
-          <div>
-            <Tabs>
-              <Tab label="COURSES">
-                <TabCourses course={course} />
-              </Tab>
-              <Tab label="HOMEWORK">
-                <TabHomework course={course} />
-              </Tab>
-              <Tab label="CALENDAR">
-                <TabCalendar course={course} />
-              </Tab>
-            </Tabs>
-          </div>
-        </div>
-      </MuiThemeProvider>
+        </MuiThemeProvider>
+      </div>
     </div>
-  </div>
-)
+  )
 }
 
 export default Page
