1723a7c26ddf1130aef7b53d2f25fcc25efa60c5
Tiago Henriques <trinosauro@gmail.com>
2017-11-27 17:17:56 +0100

    Use Link component to link to other next.js pages

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
+import Link from 'next/link'
+
 const Hello = () => <div>
     <h1>Welcome to next.js!</h1>
     <h2>Introducing concepts step by step</h2>
     <ul>
-        <li><a href="page1">Page 1</a></li>
-        <li><a href="page2">Page 2</a></li>
-        <li><a href="page3">Page 3</a></li>
-        <li><a href="page4">Page 4</a></li>
-        <li><a href="page5">Page 5</a></li>
-        <li><a href="page6">Page 6</a></li>
-        <li><a href="page7">Page 7</a></li>
-        <li><a href="page8">Page 8</a></li>
-        <li><a href="page9">Page 9</a></li>
-        <li><a href="page10">Page 10</a></li>
-        <li><a href="page11">Page 11</a></li>
-        <li><a href="page12">Page 12</a></li>
+        <li><Link href="page1"><a>Page 1</a></Link></li>
+        <li><Link href="page2"><a>Page 2</a></Link></li>
+        <li><Link href="page3"><a>Page 3</a></Link></li>
+        <li><Link href="page4"><a>Page 4</a></Link></li>
+        <li><Link href="page5"><a>Page 5</a></Link></li>
+        <li><Link href="page6"><a>Page 6</a></Link></li>
+        <li><Link href="page7"><a>Page 7</a></Link></li>
+        <li><Link href="page8"><a>Page 8</a></Link></li>
+        <li><Link href="page9"><a>Page 9</a></Link></li>
+        <li><Link href="page10"><a>Page 10</a></Link></li>
+        <li><Link href="page11"><a>Page 11</a></Link></li>
+        <li><Link href="page12"><a>Page 12</a></Link></li>
     </ul>
     <h2>Using map and flexbox</h2>
     <ul>
