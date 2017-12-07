27dcb582cc83eb68f7b39bf5bcdcc074576f254b
Tiago Henriques <trinosauro@gmail.com>
2017-12-07 14:43:22 +0100

    Reorder imports

--- pages/page13.js
+++ pages/page13.js
@@ -1,22 +1,26 @@
-import Footer from "../components/footers/footer2"
+// next.js stuff
 import Head from "next/head"
-// Material UI theme
+import Link from 'next/link'
+
+// Material UI
 import lightBaseTheme from "material-ui/styles/baseThemes/lightBaseTheme"
 import getMuiTheme from "material-ui/styles/getMuiTheme"
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
-// Material UI components
 import AppBar from "material-ui/AppBar"
 import { Tabs, Tab } from "material-ui/Tabs"
 import Paper from "material-ui/Paper"
 import RaisedButton from "material-ui/RaisedButton"
-import injectTapEventPlugin from "react-tap-event-plugin"
-
-import CourseTab from "../components/tabs/courseTab"
-import HomeworkTab from "../components/tabs/homeworkTab"
-import CalendarTab from "../components/tabs/calendarTab"
 import Drawer from "material-ui/Drawer"
 import MenuItem from "material-ui/MenuItem"
 import Subheader from "material-ui/Subheader"
+import injectTapEventPlugin from "react-tap-event-plugin"
+
+// my custom components
+import Footer from "../components/footers/footer2"
+import CourseTab from "../components/tabs/courseTab"
+import HomeworkTab from "../components/tabs/homeworkTab"
+import CalendarTab from "../components/tabs/calendarTab"
+
 
 import get from "../services/get"
 
